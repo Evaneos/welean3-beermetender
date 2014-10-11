2519a99c41a6eec558d9ecebaea2d897dcd17428
Rémi San <remi.san@gmail.com>
2014-10-11 12:42:36 +0200

    update token for each xhr request

--- app/www/js/models/beers.js
+++ app/www/js/models/beers.js
@@ -3,14 +3,16 @@ angular.module('beermetender')
 	
 		function Beers() {
 
-			var res = $resource('http://www.beer-me-tender.local/api/v1/beers/:beerId', {beerId:'@id', token: authentication.getToken()}, {query: {method: 'GET'}, 'update': { method:'PUT' }});
-
 			this.getList = function() {
+				var res = $resource('http://www.beer-me-tender.local/api/v1/beers/:beerId', {beerId:'@id', token: authentication.getToken()}, {query: {method: 'GET'}, 'update': { method:'PUT' }});
+
 				return res.query().$promise;
 			};
 
 			this.shareWithUser = function(user, balance)
 			{
+				var res = $resource('http://www.beer-me-tender.local/api/v1/beers/:beerId', {beerId:'@id', token: authentication.getToken()}, {query: {method: 'GET'}, 'update': { method:'PUT' }});
+
 				var beer = new res({
 					user_from_id: facebook.getUserID(),
 					user_to_id: user.id,
@@ -21,6 +23,8 @@ angular.module('beermetender')
 			};
 
 			this.update = function(beer) {
+				var res = $resource('http://www.beer-me-tender.local/api/v1/beers/:beerId', {beerId:'@id', token: authentication.getToken()}, {query: {method: 'GET'}, 'update': { method:'PUT' }});
+
 				return res.update({beerId: beer.id}, beer);
 			};
 
